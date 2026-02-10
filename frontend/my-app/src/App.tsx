f0f93a2c7dd4b2d8b3ff2c2c7838ea926099819f
Biswajit <paridabiswa2k@gmail.com>
2026-02-10 20:44:40 +0530

    feat:add navbar

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,23 +1,19 @@
+
 import Home from "./homepage/Home"
 import Nav from "./navbar/Nav"
 import People from "./people/People"
-import { BrowserRouter as Router, Routes, Route } from "react-router"
+import { Routes, Route } from "react-router"
 import Questions from "./questionpage/Questions"
 
 function App() {
-
-
   return (
     <>
-
-
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People />} />
         <Route path="/questions" element={<Questions />} />
-
       </Routes>
-
     </>
   )
 }
--- frontend/my-app/src/navbar/Nav.tsx
+++ frontend/my-app/src/navbar/Nav.tsx
@@ -1,17 +1,31 @@
-import Register from "../register/Register";
+import { Link, useNavigate } from "react-router"; // react-router-dom in v6, but user uses 'react-router' in imports
+import "./Nav.css";
 
+function Nav() {
+   const navigate = useNavigate();
 
-function Nav(){
-     return (
+   return (
+      <nav className="navbar">
+         <div className="navbar__logo">
+            <Link to="/">mindtrace</Link>
+         </div>
 
-        <>
-        <Register/>
+         <div className="navbar__links">
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/people" className="nav-link">People</Link>
+            <Link to="/questions" className="nav-link">Questions</Link>
+         </div>
 
-
-        
-        
-        </>
-     )
+         <div className="navbar__actions">
+            <button className="btn btn--link" onClick={() => navigate("/login")}>
+               Login
+            </button>
+            <button className="btn btn--primary btn--nav" onClick={() => navigate("/register")}>
+               Register
+            </button>
+         </div>
+      </nav>
+   );
 }
 
 export default Nav;
